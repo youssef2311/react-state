02da0e95ee5c692645b8253f5094b7fd68406636
youssef tanzekhti <tanzekhtiyoussef60@yahoo.com>
2021-02-08 03:36:09 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react'
+import Footer from './Footer/Footer';
 function App() {
+  const [show,setShow]=useState(true)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+     {
+       show?<h1>Hi my name is YoussefTanzekhti !</h1>:null
+     }
+     <br />
+    <img src="super_saiyan_god_super_saiyan_vegeta_by_moxie2d-d8p5oi6.jpg" style={{height: "250px"}} />
+<button onClick={()=>setShow(!show)} >Toggle</button> 
+<Footer />
     </div>
+   
   );
 }
 
